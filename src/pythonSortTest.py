75a861bfd0f26c639a182fef2b5bdc4a5e4a90da
nchhabra1311 <nchhabra1311@gmail.com>
2019-10-09 06:53:44 +0530

    Added radix sort in pythonSortTest.py (#37)
    * Added radix sort in pythonSortTest.py
    
    * Improved code readability as suggested

--- src/pythonSortTest.py
+++ src/pythonSortTest.py
@@ -1,4 +1,4 @@
-'''
+﻿'''
 # Create by LokiSharp(loki.sharp#gmail) at 2017-1-22
 '''
 
@@ -176,6 +176,34 @@ def countingSort(arr, maxValue=None):
             bucket[j] -= 1
     return arr
 
+def radix_count(exp1):
+    global list
+    n = len(list)
+    output = [0] * (n)
+    count = [0] * (10)
+    for i in range(0, n):
+        index = (list[i] / exp1)
+        count[(index) % 10] += 1
+    for i in range(1,10):
+        count[i] += count[i - 1]
+    i = n - 1
+    while i >= 0:
+        index = (list[i]/exp1)
+        output[count[(index) % 10] - 1] = list[i]
+        count[(index) % 10] -= 1
+        i -= 1
+    i = 0
+    for i in range(0,len(list)):
+        list[i] = output[i]
+
+def radixSort():
+    global list
+    max1 = max(list)
+    exp = 1
+    while max1 / exp > 0:
+        radix_count(exp)
+        exp *= 10
+
 
 if __name__ == '__main__':
     sortTest(bubbleSort, TOTAL)
@@ -185,3 +213,4 @@ if __name__ == '__main__':
     sortTest(mergeSort, TOTAL)
     sortTest(quickSort, TOTAL)
     sortTest(heapSort, TOTAL)
+    sortTest(radixSort, TOTAL)
